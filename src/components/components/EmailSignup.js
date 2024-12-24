3303fb48c13f2ae0d0b95caee0fa756bf3d8e180
whiteriver-dev <whiteriver.devprojects@gmail.com>
2024-12-24 15:22:04 +1100

    Started EmailSignup component

--- src/components/components/EmailSignup.js
+++ src/components/components/EmailSignup.js
@@ -5,7 +5,20 @@ import Button from './Button';
 function EmailSignup() {
   return (
     <div>
-      <Button text="Subscribe to monthly newsletter" className='subscribe-btn' />
+      <form className='email-signup-form' onSubmit={handleSubmit}>
+        <label htmlFor='email-input' className='email-input-label'>
+          Email Address
+        </label>
+        <input
+          type='email'
+          id='email-input'
+          className='email-input'
+          placeholder='email@company.com'
+          value={email}
+        
+        />
+        <Button text='Subscribe to monthly newsletter' className='subscribe-btn' />
+      </form>
     </div>
   );
 }
