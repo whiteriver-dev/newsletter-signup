7aa47ebd40f43f8d64624f17509052839610a8f5
Igor Dubik <whiteriver.devprojects@gmail.com>
2024-12-27 19:58:11 +1100

    Added content to NewsletterSignupTemplate

--- src/components/templates/NewsletterSignupTemplate.js
+++ src/components/templates/NewsletterSignupTemplate.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { EmailSignup } from '..';
 
+// This is a non-reusable component
+
 function NewsletterSignupTemplate() {
     return (
-        <EmailSignup/>
+        <div>
+            <h1>Stay updated!</h1>
+            <p>Join 60,000+ product managers receiving monthly updates on:</p>
+            <ul>
+                <li>Product discovery and building what matters</li>
+                <li>Measuring to ensure updates are a success</li>
+                <li>And much more!</li>
+            </ul>
+            <EmailSignup/>
+        </div>
     )
 }
 
